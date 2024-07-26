f6dfdedf29e68363e62ca8874e75548c484752d9
haydeniw <haydenwolff99@gmail.com>
2024-07-26 20:12:55 +0000

    updated chain

--- code/chain_server/chain.py
+++ code/chain_server/chain.py
@@ -45,6 +45,7 @@ retriever = vector_store.as_retriever()
 
 def format_docs(docs: list[Document]) -> str:
     """Take in a list of docs and concatenate the content, separating by newlines."""
+    print("formatting docs")
     return "\n\n".join(doc.page_content for doc in docs)
 
 
