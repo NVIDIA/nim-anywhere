944592072cb0291bd4bb9aa1882684fe9d493399
Ryan Kraus <rkraus@nvidia.com>
2024-08-02 21:42:04 +0000

    Bumped dependencies to latest.

---
+++ code/tools/bump.sh
@@ -0,0 +1,94 @@
+#!/bin/bash
+set -e
+cd /project
+
+
+# helpers
+function _divider() {
+	echo -e "\n\n\n\n\n"
+}
+
+# python deps
+function _bump_reqs() {
+	echo "Updating Python Dependencies"
+	pip install bumper --upgrade > /dev/null
+	bump
+}
+
+# nims
+function _bump_nims() {
+	echo "Updating NIMs"
+	for app in apps/*.sh; do
+		# get application image
+		export IMAGE=$(env -i /bin/bash $app image 2> /dev/null)
+		export TAG=$(env -i /bin/bash $app tag 2> /dev/null)
+
+		# ensure this is a nim
+		if [[ "$IMAGE" != "nvcr.io/nim/"* ]]; then
+			continue
+		fi
+
+		# find the latest tag
+		LATEST=$( \
+			skopeo list-tags docker://$IMAGE | \
+			jq '.Tags' | \
+			jq -r '.[] | select(contains("sha256") | not)' | \
+			grep -v 'latest' | \
+			tail -n 1)
+
+		# check if an update is required
+		if [ "${LATEST}x" != "${TAG}x" ]; then
+			echo "Updating: $app [ $TAG -> $LATEST ]"
+			sed -i -r 's/(^TAG=.*)"'$TAG'"\)/\1"'$LATEST'")/g' "$app"
+			git diff "$app"
+		else
+			echo "NO CHANGE"
+			echo "$TAG == $LATEST"
+		fi
+		echo ""
+	done
+}
+
+# milvus
+
+main() {
+	_bump_reqs
+	_divider
+	_bump_nims
+}
+
+# apps
+echo "Updating NIMs"
+for app in apps/*.sh; do
+	export IMAGE=$(env -i /bin/bash $app image 2> /dev/null)
+	export TAG=$(env -i /bin/bash $app tag 2> /dev/null)
+
+	# ensure this is a nim
+	if [[ "$IMAGE" != "nvcr.io/nim/"* ]]; then
+		continue
+	fi
+	echo $app
+
+	# find the latest tag
+	LATEST=$( \
+		skopeo list-tags docker://$IMAGE | \
+		jq '.Tags' | \
+		jq -r '.[] | select(contains("sha256") | not)' | \
+		grep -v 'latest' | \
+		tail -n 1)
+
+	# check if an update is required
+	if [ "${LATEST}x" != "${TAG}x" ]; then
+		echo "UPDATING!"
+		echo "$TAG -> $LATEST"
+
+		sed -i -r 's/(^TAG=.*)"'$TAG'"\)/\1"'$LATEST'")/g' "$app"
+		git diff "$app"
+	else
+		echo "NO CHANGE"
+		echo "$TAG == $LATEST"
+	fi
+	echo ""
+done
+
+
