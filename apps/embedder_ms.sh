77bff04642ece04ee23be1c383ebbce9f80709ad
Amelia <ameliay@nvidia.com>
2024-11-25 16:28:07 -0800

    Dependency Bumping (#64)
    Co-authored-by: GitHub Actions <actions@github.com>
    Co-authored-by: Amelia Taihui Ye <taihui@umich.edu>

--- apps/embedder_ms.sh
+++ apps/embedder_ms.sh
@@ -25,7 +25,7 @@ NAME="${SVC_NAME}"
 
 # workspace configuration options
 MODEL=$(config_lkp "${SLUG}_MODEL" "nvidia/nv-embedqa-e5-v5")
-TAG=$(config_lkp "${SLUG}_NIM_VERSION" "1.0.1")
+TAG=$(config_lkp "${SLUG}_NIM_VERSION" "1")
 GPUS=$(config_lkp "${SLUG}_NIM_GPUS" "all")
 IMAGE="nvcr.io/nim/$MODEL"
 
